87c3c9ba3d7d1fb49409ddc2c7f4214a33f449b9
DESKTOP-F94F91R\prade <fathiarchu@gmail.com>
2021-01-23 18:33:24 +0800

    changes

--- src/Sample.java
+++
@@ -1,6 +0,0 @@
-public class Sample {
-       import com.sun.org.apache.xalan.internal.xsltc.compiler.XSLTC;
-
-import java.net.URL;
-
-}
--- src/test/java/FacebookTest.java
+++ src/test/java/FacebookTest.java
@@ -1,57 +1,93 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.io.*;
+import java.util.NoSuchElementException;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class FacebookTest {
 
     private static final String URL = "https://www.newegg.com/NVIDIA/BrandStore/ID-1441";
-    public static void main (String [] args) {
-        WebDriver driver=null;
+
+    public static void main(String[] args) {
+        WebDriver driver;
         String projectPath = System.getProperty("user.dir");
-        System.setProperty("webdriver.chrome.driver", projectPath+"/src/test/drivers/chromedriver.exe");
-
-
+        System.setProperty("webdriver.chrome.driver", projectPath + "/src/test/drivers/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.navigate().to(URL);
         driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-        // driver.findElement(By.xpath("//div[contains(text(),'North America')]")).click();
-        driver.findElement(By.xpath("//div[contains(text(),'North America')]")).click();
-        driver.findElement(By.xpath("/html/body/div[10]/div/div/div/div[2]/div[1]/div[2]/div/div/div/ul/li[3]/div")).click();
-        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//button[contains(text(),'Stay at United States')]")).click();
-        driver.manage().timeouts().pageLoadTimeout(40,TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+
+        WebElement centerPopUp = driver.findElement(By.xpath("//*[@class='centerPopup-body']"));
+        System.out.println(centerPopUp.isDisplayed());
+
+        driver.findElement(By.xpath("//*[text()='North America']")).click();
+        driver.findElement(By.xpath("//*[text()='United States']")).click();
+        driver.findElement(By.xpath("//button[text()='Stay at United States']")).click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.findElement(By.id("tb_id109")).click();
-        driver.manage().timeouts().pageLoadTimeout(40,TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+        int countOfProducts = driver.findElements(By.xpath("//*[@class='pageItemContainer']")).size();
+        System.out.println(countOfProducts);
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,350)", "");
-        for(int i=1;i<5;i++){
+
+       for(int i=1;i<5;i++){
             try
             {
 
-                String productName =driver.findElement(By.xpath("//*[@id=\"tbc_id109\"]/div[1]/div["+i+"]/div/div[1]")).getText();
+                String productName =driver.findElement(By.xpath("(//*[@class=\"itemTitle\"])["+i+"]")).getText();
 
                 System.out.println("Product Name in the first row: " + productName);
 
-                String price =driver.findElement(By.xpath("//*[@id=\"tbc_id109\"]/div[1]/div["+i+"]/div/div[2]")).getText();
+                String price =driver.findElement(By.xpath("(//*[@class=\"itemPrice\"])["+i+"]")).getText();
                 System.out.println("Unit price of the items in the first row" + price);
 
             }
 
-            catch(Exception e)
+            catch(NoSuchElementException e)
             {
-                break;
+                e.printStackTrace();
             }
 
         }
+        String firstPrice = driver.findElement(By.xpath("(//*[@class=\"itemPrice\"])[1]")).getText();
+
+        System.out.println("Price in main Window" + firstPrice);
+
+        driver.findElement(By.xpath("(//*[@class=\"buton btnBrand\"])[1]")).click();
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+
+        Set<String> allWindowHandles = driver.getWindowHandles();
+        System.out.println("Handles " + allWindowHandles);
+        for (String handle : allWindowHandles) {
+            System.out.println("Switching to window - > " + handle);
+            try {
+
+                driver.switchTo().window(handle);
+            }
+            catch(NoSuchWindowException e) {
+                e.printStackTrace();
+            }
+        }
+        WebElement text = driver.findElement(By.xpath("(//*[@class=\"price-current\"])[1]"));
+
+        String detailedPrice = text.getText();
+        System.out.println("Price in detailed session " + detailedPrice);
+        if (detailedPrice.equalsIgnoreCase(firstPrice)) {
+            System.out.println("Price in child window is matching with product list ");
+        } else {
+            System.out.println("Price in child window is not matching with product list ");
+        }
+        driver.close();
+        driver.quit();
     }
 
-
 }
+
+
+
+
+
+
+
